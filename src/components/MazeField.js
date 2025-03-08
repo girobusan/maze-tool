ddd7b91b091bc792489efed816349d4b3d45ad29
Girobusan <private>
2025-03-08 10:26:51 +0300

    more work

--- src/components/MazeField.js
+++ src/components/MazeField.js
@@ -7,20 +7,19 @@ import { seed2pos, randomPos } from "../util";
 const d3 = require("d3");
 const mod = 32;
 
-function drawField(cont, module, seedcont, seed) {
-  const groups = d3
-    .select(cont)
+function drawField(pos, cont, pawnscont) {
+  d3.select(cont)
     .selectAll("g.field")
     .data(Field)
     .join(
       (enter) => {
         const z = enter.append("g");
         z.attr("class", "field").attr("transform", (d) => {
-          return `translate(${(d.x - 1) * module} ${(d.y - 1) * module})`;
+          return `translate(${(d.x - 1) * mod} ${(d.y - 1) * mod})`;
         });
         z.append("rect")
-          .attr("width", module)
-          .attr("height", module)
+          .attr("width", mod)
+          .attr("height", mod)
           .attr("fill", (d) => (isBlack(d.x, d.y) ? "#eee" : "white"))
           .attr("stroke-width", 1)
           .attr("stroke", "gray");
@@ -32,11 +31,11 @@ function drawField(cont, module, seedcont, seed) {
       },
       (exit) => exit.remove(),
     );
-  const pos = seed ? seed2pos(seed) : [];
+  // const pos =  pos;
 
-  d3.select(seedcont)
+  d3.select(pawnscont)
     .selectAll("g.pawn")
-    .data(pos)
+    .data(pos || [])
     .join(
       (enter) => {
         let z = enter.append("g");
@@ -52,7 +51,7 @@ function drawField(cont, module, seedcont, seed) {
       },
     )
     .attr("transform", (d) => {
-      return `translate(${(d.x - 1) * module} ${(d.y - 1) * module})`;
+      return `translate(${(d.x - 1) * mod} ${(d.y - 1) * mod})`;
     })
     .attr("data-piece", (d) => d.piece.name)
     .append("text")
@@ -64,16 +63,15 @@ function drawField(cont, module, seedcont, seed) {
     .text((d) => d.piece.icon);
 }
 
-export function MazeField({ seed = "helloy" }) {
+export function MazeField({ pos }) {
   const FLDS = useRef(null);
   const PWNS = useRef(null);
   useEffect(() => {
     if (!FLDS.current || !PWNS.current) {
       return;
     }
-    drawField(FLDS.current, mod, PWNS.current, seed);
-    console.log(seed2pos(seed));
-  }, [FLDS, FLDS.current, PWNS, PWNS.current]);
+    drawField(pos, FLDS.current, PWNS.current);
+  }, [FLDS, FLDS.current, PWNS, PWNS.current, pos]);
 
   return html`<svg
     viewBox="0 0 ${mod * 10 + 2} ${mod * 6 + 2}"
