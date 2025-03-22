9de541555bc9a2a4c5d4efe612a838423627dd7d
Girobusan <private>
2025-03-22 19:48:06 +0300

    adding embed and notes

---
+++ docs/helpers/autoload.js
@@ -0,0 +1,22 @@
+window.impHelpers.autoload = {
+  message: "Test autoload setup",
+  helpers: [
+    [
+      "navbar",
+      ,
+      {
+        logo: "<strong>Level generator</strong>",
+        link: "index.html",
+        //exclude: ["index.html$"],
+        links: [
+["https://github.com/girobusan/maze-tool" , "maze-tool@github"],
+          
+          ["https://girobusan.github.io/", "@girobusan"],
+["maze_notes.html", "Notes on 'Maze'"],
+        ],
+      },
+    ],
+    
+   
+  ],
+};
